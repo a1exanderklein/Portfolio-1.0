9cc6084cd55707c62c279b5cf8551e19c6bb792a
a1exanderklein <agklein04@outlook.com>
2023-08-23 13:49:04 -0400

    Added Pages

---
+++ script.js
@@ -0,0 +1,11 @@
+function openLinkedIn() {
+    window.open("https://www.linkedin.com/in/alexanderkleincs/");
+}
+
+function openGithub() {
+    window.open("https://github.com/a1exanderklein");
+}
+
+function openEmail() {
+    window.open("mailto:alexandergeorgeklein@gmail.com");
+}
